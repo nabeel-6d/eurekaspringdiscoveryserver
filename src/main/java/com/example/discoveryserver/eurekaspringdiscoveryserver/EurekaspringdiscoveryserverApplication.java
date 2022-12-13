6fb278120fb337afba7bf91607e8a4f25dee11df
nabeel-6d <mahmood.nabeel@6dtech.co.in>
2022-12-13 18:35:35 +0530

    discoveryserver

--- src/main/java/com/example/discoveryserver/eurekaspringdiscoveryserver/EurekaspringdiscoveryserverApplication.java
+++ src/main/java/com/example/discoveryserver/eurekaspringdiscoveryserver/EurekaspringdiscoveryserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaspringdiscoveryserverApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaspringdiscoveryserverApplication.class, args);
 	}
