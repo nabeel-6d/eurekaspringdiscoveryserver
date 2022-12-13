50cc2bde181e876bedeba92652c5de33fcd0331f
nabeel-6d <mahmood.nabeel@6dtech.co.in>
2022-12-13 12:08:00 +0530

    eureka server

---
+++ src/main/java/com/example/discoveryserver/eurekaspringdiscoveryserver/EurekaspringdiscoveryserverApplication.java
@@ -0,0 +1,15 @@
+package com.example.discoveryserver.eurekaspringdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaspringdiscoveryserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaspringdiscoveryserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/discoveryserver/eurekaspringdiscoveryserver/EurekaspringdiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryserver.eurekaspringdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaspringdiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
